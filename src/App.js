e04cb3945e4475e3879ff8f266f6b9d4e8bb532b
unknown <xiongjoh@gmail.com>
2020-10-09 12:47:38 -0500

    add characters.js finished mvp

--- src/App.js
+++ src/App.js
@@ -1,17 +1,50 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import Character from './components/Character'
+import axios from 'axios'
+import styled from 'styled-components'
+
+// Style for App
+const StyledPokemonContainer = styled.div`
+display:flex;
+flex-flow: row wrap;
+justify-content: center;
+`
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
+  const [characterList, setCharacterList] = useState([])
+
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+
+
+  useEffect(() => {
+    axios.get('https://pokeapi.co/api/v2/pokemon?limit=5&offset=1')
+    .then(res => {
+      // console.log(res.data.results.forEach(name => console.log(name)))
+      res.data.results.forEach(pokemon => {
+        setCharacterList(array => [...array, pokemon])
+      })
+    })
+    .catch(res => {
+      debugger
+    })
+  }, [])
+
+
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      <h1 className="Header">Pokemon</h1>
+      <StyledPokemonContainer>
+      {characterList && characterList.map(character => <Character name={character.name} url={character.url}/>)}
+      </StyledPokemonContainer>
+      
+
     </div>
   );
 }
